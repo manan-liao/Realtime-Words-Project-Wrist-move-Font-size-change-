06d2fe964c20fa06fab6f303901f0ed9e4064822
manan-liao <77179093+manan-liao@users.noreply.github.com>
2021-10-13 16:17:48 +1100

    Add files via upload

--- main.js
+++ main.js
@@ -1,7 +1,13 @@
+noseX = 0;
+noseY = 0;
+difference = 0;
+rightWristX = 0;
+leftWristX = 0;
+
 function setup(){
     video = createCapture(VIDEO);
     video.size(550,500);
-    canvas = createCanvas(550,500);
+    canvas = createCanvas(750,500);
     canvas.position(600,120);
     poseNet = ml5.poseNet(video,modelLoaded);
     poseNet.on("pose",gotPoses);
@@ -9,6 +15,9 @@ function setup(){
 
 function draw(){
     background("lightgreen");
+    textSize(difference);
+    fill("blue");
+    text("Manan",noseX,noseY);
 }
 
 function modelLoaded(){
@@ -18,5 +27,11 @@ function modelLoaded(){
 function gotPoses(results){
     if(results.length>0){
         console.log(results);
+        noseX = results[0].pose.nose.x;
+        noseY = results[0].pose.nose.y;
+        rightWristX = results[0].pose.rightWrist.x;
+        leftWristX = results[0].pose.leftWrist.x;
+        console.log("NoseX = " + noseX + ",NoseY = " + noseY);
+        difference = floor(leftWristX - rightWristX);
     }
 }
